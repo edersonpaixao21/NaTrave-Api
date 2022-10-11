f650b3b158dc5053cb60feecaf95e43f9b85c65d
edersonpaixao21 <ederson.cpds@gmail.com>
2022-10-11 16:18:15 -0300

    first commit

---
+++ app/hunches/index.js
@@ -0,0 +1,43 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export const create = async ctx => {
+    if (!ctx.request.body.homeTeamScore && !ctx.request.body.awayTeamScore) {
+        ctx.status = 400
+        return
+    }
+
+    const userId = 'cl91szw9s0000uhfs790f4eo4'
+    const { gameId } = ctx.request.body
+    const homeTeamScore = parseInt(ctx.request.body.homeTeamScore)
+    const awayTeamScore = parseInt(ctx.request.body.awayTeamScore)
+
+    try {
+        const [hunch] = await prisma.hunch.findMany({
+            where: { userId, gameId },
+        })
+
+        ctx.body = hunch
+            ? await prisma.hunch.update({
+                where: {
+                    id: hunch.id
+                },
+                data: {
+                    homeTeamScore,
+                    awayTeamScore
+                }
+            })
+            : await prisma.hunch.create({
+                data: {
+                    userId,
+                    gameId,
+                    homeTeamScore,
+                    awayTeamScore
+                }
+            })
+    } catch (error) {
+        console.log(error)
+        ctx.body = error
+        ctx.status = 500
+    }
+}
